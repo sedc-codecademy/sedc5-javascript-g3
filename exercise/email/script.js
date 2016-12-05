12c16637e9e56fca807500803df24e2d9ce36863
Wekoslav Stefanovski <swekster@gmail.com>
2016-12-05 21:41:56 +0100

    Added getDomain implementation

--- exercise/email/script.js
+++ exercise/email/script.js
@@ -1,6 +1,22 @@
 function getDomain(email) {
-    let indexOfAt = email.indexOf("@")+1;
-    let domain = email.substr(indexOfAt);
+    let indexOfAt = email.indexOf("@");
+    if (indexOfAt <= 0)
+        return false;
+    if (email.lastIndexOf("@") !== indexOfAt)
+        return false;
 
+    let domain = email.substr(indexOfAt + 1);
     return domain;
+}
+
+function isSameDomain(first, second){
+    var firstDomain = getDomain(first);
+    if (!firstDomain)
+        return false;
+    var secondDomain = getDomain(second);
+    if (!secondDomain)
+        return false;
+
+    let result = firstDomain === secondDomain;
+    return result;
 }
