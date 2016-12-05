2f7e9814a3f410bd534473f1e58ae21b1bd526a3
Wekoslav Stefanovski <swekster@gmail.com>
2016-12-05 19:11:35 +0100

    add

--- exercise/addition/script.js
+++ exercise/addition/script.js
@@ -1,3 +1,13 @@
-function additionWithoutCarrying(param1, param2){
-       
+function additionWithoutCarrying(param1, param2) {
+
+}
+
+function numberToDigits(number) {
+    var digits = number.toString().split("").map(n => parseInt(n));
+    return digits;
+}
+
+function digitsToNumber(digits) {
+    var number = parseInt(digits.join(""));
+    return number;
 }
