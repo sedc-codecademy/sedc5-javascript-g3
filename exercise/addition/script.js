b600471720ee17756fc203cfb18a7d4d8b8f7d02
Wekoslav Stefanovski <swekster@gmail.com>
2016-12-05 20:18:28 +0100

    Addition example

--- exercise/addition/script.js
+++ exercise/addition/script.js
@@ -1,5 +1,34 @@
 function additionWithoutCarrying(param1, param2) {
+    let oneDigits = numberToDigits(param1);
+    let twoDigits = numberToDigits(param2);
 
+    let length = Math.max(oneDigits.length, twoDigits.length);
+    console.log(length);
+
+    padZero(oneDigits, length);
+    padZero(twoDigits, length);
+
+    console.log(oneDigits);
+    console.log(twoDigits);
+
+    let result = [];
+    for (let index = 0; index < length; index++) {
+        let first = oneDigits[index];
+        let second = twoDigits[index];
+        let resDigit = first + second;
+        if (resDigit >= 10) resDigit -= 10;
+        result.push(resDigit);
+    }
+
+    let sum = digitsToNumber(result);
+    return sum;
+}
+
+function padZero(array, length) {
+    if (array.length < length) {
+        while (array.length < length)
+            array.unshift(0);
+    }
 }
 
 function numberToDigits(number) {
@@ -10,4 +39,15 @@ function numberToDigits(number) {
 function digitsToNumber(digits) {
     var number = parseInt(digits.join(""));
     return number;
+}
+
+
+function calculate(){
+    var first = document.getElementById("first").valueAsNumber;
+    var second = document.getElementById("second").valueAsNumber;
+    var result = document.getElementById("result");
+
+    var value = additionWithoutCarrying(first, second);
+
+    result.innerHTML = value;
 }
--- exercise/addition/tests.js
+++ exercise/addition/tests.js
@@ -9,3 +9,34 @@ QUnit.test("addition for 7 and 8", function (assert) {
     let expected = 5;
     assert.equal(expected, result);
 });
+
+QUnit.test("addition for 456 and 1734", function (assert) {
+    let result = additionWithoutCarrying(456, 1734);
+    let expected = 1180;
+    assert.equal(expected, result);
+});
+
+QUnit.test("addition for 78 and 56789", function (assert) {
+    let result = additionWithoutCarrying(78, 56789);
+    let expected = 56757;
+    assert.equal(expected, result);
+});
+
+QUnit.test("addition for 56789 and 78", function (assert) {
+    let result = additionWithoutCarrying(56789, 78);
+    let expected = 56757;
+    assert.equal(expected, result);
+});
+
+QUnit.test("addition for 50 and 50", function (assert) {
+    let result = additionWithoutCarrying(50, 50);
+    let expected = 0;
+    assert.equal(expected, result);
+});
+
+QUnit.test("addition for 501 and 503", function (assert) {
+    let result = additionWithoutCarrying(501, 503);
+    let expected = 4;
+    assert.equal(expected, result);
+});
+
